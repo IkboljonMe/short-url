1b0ca0f89d4195367219c081a9bb7954e6208987
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-24 01:02:56 +0200

    User model

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+UserSchema.methods.generateJwtFromUser = function () {
+  const { JWT_SECRET_KEY, JWT_EXPIRE } = process.env;
+  payload = {
+    id: this._id,
+    username: this.username,
+    email: this.email,
+  };
+  const token = jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: JWT_EXPIRE });
+  return token;
+};
+module.exports = mongoose.model("User", UserSchema);
