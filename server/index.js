b89d24d6a68dd04b6bb51fb082b4f7a42fe19c62
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-23 18:40:09 +0200

    clean up

--- server/index.js
+++ server/index.js
@@ -6,73 +6,16 @@ const shortid = require("shortid");
 const utils = require("./utils/utils");
 const Url = require("./models/Urls");
 const bodyParser = require("body-parser");
-
+const databaseConnection = require("./db/databaseConnection");
+const IndexRoute = require("./routes/index");
 dotenv.config();
 const app = express();
-
+//db connection
+databaseConnection();
 app.use(cors());
 app.use(bodyParser());
-
 app.use(express.json());
-
-//db connection
-
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(console.log("DB connected"))
-  .catch((err) => {
-    console.log(err.message);
-  });
-//post endpoint
-app.post("/", async (req, res, next) => {
-  console.log("URL HERE", req.body.url);
-  const { origUrl } = req.body;
-  const base = `http://localhost:3333`;
-  const urlId = shortid.generate();
-  if (utils.validateUrl(origUrl)) {
-    try {
-      let url = await Url.findOne({ origUrl });
-      if (url) {
-        res.json(url);
-      } else {
-        const shortUrl = `${base}/${urlId}`;
-        const url = new Url({
-          origUrl,
-          shortUrl,
-          urlId,
-          data: Date.now(),
-        });
-        await url.save();
-        res.json(url);
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(500).json("Server Error");
-    }
-  } else {
-    res.status(400).json("Invalid Original Url");
-  }
-});
-//redirect endpoint
-app.get("/:urlId", async (req, res) => {
-  try {
-    const url = await Url.findOne({ urlId: req.params.urlId });
-    console.log("REDIRECT URL", url);
-    if (url) {
-      url.clicks++;
-      url.save();
-      return res.redirect(url.origUrl);
-    } else {
-      return res.status(404).json("Not found");
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json("Server Error");
-  }
-});
+app.use("/", IndexRoute);
 
 // Port Listenning on 3333
 const PORT = process.env.PORT || 3333;
