b1b5b201d0c94874f100c919fc53e84f22660ca3
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-23 18:39:30 +0200

    url controller

---
+++ server/controllers/url.js
@@ -0,0 +1,50 @@
+const Url = require("../models/Urls");
+const shortid = require("shortid");
+const utils = require("../utils/utils");
+
+const shortenUrl = async (req, res, next) => {
+  console.log("URL HERE", req.body.url);
+  const { origUrl } = req.body;
+  const urlId = shortid.generate();
+  if (utils.validateUrl(origUrl)) {
+    try {
+      let url = await Url.findOne({ origUrl });
+      if (url) {
+        res.json(url);
+      } else {
+        const shortUrl = `${process.env.BASE}/${urlId}`;
+        const url = new Url({
+          origUrl,
+          shortUrl,
+          urlId,
+          data: Date.now(),
+        });
+        await url.save();
+        res.json(url);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json("Server Error");
+    }
+  } else {
+    res.status(400).json("Invalid Original Url");
+  }
+};
+const getShortenUrl = async (req, res) => {
+  try {
+    const url = await Url.findOne({ urlId: req.params.urlId });
+    console.log("REDIRECT URL", url);
+    if (url) {
+      url.clicks++;
+      url.save();
+      return res.redirect(url.origUrl);
+    } else {
+      return res.status(404).json("Not found");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("Server Error");
+  }
+};
+
+module.exports = { shortenUrl, getShortenUrl };
