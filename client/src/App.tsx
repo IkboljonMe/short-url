91ee361bbbb36ba668c5ff578326ad1d7fedf2fb
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-25 01:15:28 +0200

    profile commits

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import UrlShortener from "./components/UrlShortener";
 import { Register, Login } from "./components/Auth/";
+import Profile from "./components/Profile";
 function App() {
   return (
     <Router>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/short" element={<UrlShortener />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
